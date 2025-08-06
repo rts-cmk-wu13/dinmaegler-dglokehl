e0e47506ca2c932e6a3e273f2fbe6f0a9de292d3
Loke <U2507983@rts-365.dk>
2025-08-06 11:00:43 +0200

    added section

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,12 @@
 import PageWrapper from "@/components/PageWrapper";
 
+import { FaHouseLaptop, FaHouseMedical, FaLocationDot, FaUsersBetweenLines } from "react-icons/fa6";
+import { BsBuildingsFill } from "react-icons/bs";
+
 
 export default function Home() {
     return (
-        <PageWrapper>
+        <PageWrapper className="flex flex-col *:w-full *:self-center">
             <div className="h-[698px] flex justify-center items-center relative">
                 <div className="max-w-4xl w-full absolute z-9">
                     <h1 className="text-5xl font-bold text-c-white text-center mb-10">Søg efter din drømmebolig</h1>
@@ -15,7 +18,7 @@ export default function Home() {
 
                         <hr className="mt-1 mb-5" />
 
-                        <label htmlFor="herosearch" className="mb-1 text-body-2">
+                        <label htmlFor="herosearch" className="pb-1 block text-body-2 text-c-body-1">
                             Hvad skal din næste bolig indeholde?
                         </label>
                         <div className="h-12 flex gap-2.5 text-body-2 *:rounded-xs">
@@ -27,6 +30,120 @@ export default function Home() {
 
                 <img src="/hero_house.jpg" alt="" className="size-full object-cover brightness-50" />
             </div>
+
+
+            <div className="py-32 max-w-maxw-default">
+                <section className="flex gap-32">
+                    <div className="relative size-[448px]">
+                        <div className="absolute top-8 left-8 z-9 size-full">
+                            <div className="size-full relative inset-shadow-[0_0_0_12px] inset-shadow-c-primary-1">
+                                <div className="size-[210px] absolute bottom-0 flex flex-col justify-center items-center gap-2 right-0 bg-c-primary-1 text-c-white text-center">
+                                    <p className="text-6xl font-bold">
+                                        38+
+                                    </p>
+                                    <p className="text-2xl">
+                                        års mægler-<br />erfaring
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+
+                        <img src="/familyphoto.jpg" alt="" className="size-full object-cover" />
+                    </div>
+
+                    <div className="w-full flex-1">
+                        <h2 className="mb-8 text-heading-1 text-c-heading-1">
+                            Vi har fulgt danskerne hjem i snart 4 årtier
+                        </h2>
+
+                        <h3 className="mb-3 text-heading-3 text-c-primary-1">
+                            Det synes vi siger noget om os!
+                        </h3>
+                        <p className="mb-5 text-body-1 text-c-body1">
+                            It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has normal distribution.
+                        </p>
+                        <p className="text-body-1 text-c-body1">
+                            It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.
+                        </p>
+
+                        <div className="mt-10 flex justify-between items-center">
+                            <div className="flex gap-5">
+                                <div className="size-16 grid place-items-center bg-c-shape-2">
+                                    <FaHouseLaptop className="size-10 text-c-primary-1" />
+                                </div>
+                                <div>
+                                    <h3 className="text-heading-3 text-c-heading-2">
+                                        4829
+                                    </h3>
+                                    <p className="text-body-1 text-c-body-1">
+                                        boliger solgt
+                                    </p>
+                                </div>
+                            </div>
+                            <div className="flex gap-5">
+                                <div className="size-16 grid place-items-center bg-c-shape-2">
+                                    <FaHouseMedical className="size-10 text-c-primary-1" />
+                                </div>
+                                <div>
+                                    <h3 className="text-heading-3 text-c-heading-2">
+                                        158
+                                    </h3>
+                                    <p className="text-body-1 text-c-body-1">
+                                        boliger til salg
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </section>
+
+                <hr className="my-16" />
+
+                <div className="grid grid-cols-3 gap-8">
+                    <article className="flex gap-3">
+                        <div className="size-12 grid place-items-center bg-c-shape-2">
+                            <BsBuildingsFill className="size-9 text-c-primary-1" />
+                        </div>
+                        <div className="flex-1">
+                            <h3 className="text-heading-3 text-c-heading-2">
+                                Bestil et salgstjek
+                            </h3>
+                            <p className="mt-4 text-body-1 text-c-body-1">
+                                Med et Din Mægler Salgstjek bliver du opdateret på værdien af din bolig.
+                            </p>
+                        </div>
+                    </article>
+
+                    <article className="flex gap-3">
+                        <div className="size-12 grid place-items-center bg-c-shape-2">
+                            <FaLocationDot className="size-9 text-c-primary-1" />
+                        </div>
+                        <div className="flex-1">
+                            <h3 className="text-heading-3 text-c-heading-2">
+                                74 butikker
+                            </h3>
+                            <p className="mt-4 text-body-1 text-c-body-1">
+                                Hos Din Mægler er din bolig til salg i alle vores 74 butikker, som er fordelt rundt om i Danmark.
+                            </p>
+                        </div>
+                    </article>
+
+                    <article className="flex gap-3">
+                        <div className="size-12 grid place-items-center bg-c-shape-2">
+                            <FaUsersBetweenLines className="size-9 text-c-primary-1" />
+                        </div>
+                        <div className="flex-1">
+                            <h3 className="text-heading-3 text-c-heading-2">
+                                Tilmeld køberkartotek
+                            </h3>
+                            <p className="mt-4 text-body-1 text-c-body-1">
+                                Når du er tilmeldt vores køberkartotek, bliver du kontaktet inden en ny bolig bliver annonceret.
+                            </p>
+                        </div>
+                    </article>
+                </div>
+            </div>
+
         </PageWrapper>
     );
 }
